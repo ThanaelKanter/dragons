034b259176823c0196b740c2526c5231aad8611b
Thanael <thanael@visitor-10-59-136-162.wifi.berkeley.edu>
2024-01-30 18:34:01 -0800

    already wrote out a thing here I had to update my file so not sure if the other will pop up but this file has nothing to do with Smaug or any other type of dragon.

--- smaug.py
+++ smaug.py
@@ -1,2 +1,3 @@
 print (23456*445671*956000*324*0) #this number represents how many braincells I have working on a daily basis.
 print ("Not sure if you can see the comment but I said that this is the number of braincells I have working on a daily basis.")
+#the command I have used to run this file in my terminal is 'python smaug.py'
