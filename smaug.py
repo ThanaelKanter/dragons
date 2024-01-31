700032c4ca3655d65c6f8cd498bee7e4711b4c41
Thanael <thanael@visitor-10-59-136-162.wifi.berkeley.edu>
2024-01-30 18:24:16 -0800

    smaug.py has nothing to do with dragons or the specific dragon Smaug but it does have some nifty stuff.

---
+++ smaug.py
@@ -0,0 +1,2 @@
+print (23456*445671*956000*324*0) #this number represents how many braincells I have working on a daily basis.
+print ("Not sure if you can see the comment but I said that this is the number of braincells I have working on a daily basis.")
